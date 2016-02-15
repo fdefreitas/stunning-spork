6404fa9a463184ed1445a19af2f49d5d2422cf9b
Fernando De Freitas <fdefreitas6@gmail.com>
2016-02-15 02:11:42 +0100

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fantooAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fantooAppApp
+ */
+angular.module('fantooAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fantooAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fantooAppApp
+ */
+angular.module('fantooAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
